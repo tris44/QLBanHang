595d46df92bb176d036be0ad64d25c7ee1c84dd2
Tri Nguyen <mistino26@gmail.com>
2026-06-17 16:08:19 +0700

    hoan thien main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
-// TODO: hoàn thiện sau khi tích hợp toàn bộ module
+#include "../include/menu.hpp"
+#include "../include/datamanager.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Thông tin sinh viên thực hiện
+    cout << "Nguyễn Quang Huy - 202418920\n";
+    cout << "Nguyễn Minh Trí - 202419000\n";
+    cout << "Hoàng Nguyên Vũ - 202419020\n";
+    cout << "Chuong trinh Quan ly Ban hang\n";
+    cout << "========================================\n\n";
+
+    // Tải dữ liệu từ 4 file .txt vào bộ nhớ
+    loadData();
+
+    // Hiển thị menu chính, chương trình chạy tới khi người dùng chọn Thoát
+    mainMenu();
+
+    return 0;
+}
